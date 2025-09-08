c83fe471e8e1d4e65d1e605802a3319498987586
igor2000xp <igorring@google.com>
2025-09-08 17:54:41 +0200

    feat: enhance country and state selection functionality with reactive forms and improved state management

--- src/app/exercise2/exercise2.component.ts
+++ src/app/exercise2/exercise2.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { combineLatest, map, Observable, startWith, switchMap, tap } from 'rxjs';
 import { CountryService } from './country.service';
-import { Country } from './types';
+import { Country, State } from './types';
 
 @Component({
   selector: 'app-exercise2',
@@ -10,8 +10,29 @@ import { Country } from './types';
   styleUrls: ['./exercise2.component.css'],
 })
 export class Exercise2Component {
+  countryDropdown = new FormControl<Country['id']>('');
+  statesDropdown = new FormControl<State['code']>('');
+
   countries$: Observable<Country[]> = this.service.getCountries();
-  countryDropdown = new FormControl<Country['id']>(null);
+  states$: Observable<State[]> = this.service.getStates('');
+
+  currentCountry$ = combineLatest([
+    this.countries$,
+    this.countryDropdown.valueChanges.pipe(startWith(this.countryDropdown.value))
+  ]).pipe(map(([countries, countryID]) => {
+    return countries.find((c) => c.id === countryID)?.id;
+  }));
+
+  statesList$ = this.currentCountry$.pipe(
+    switchMap((countryID) => {
+      return this.service.getStates(countryID);
+    })
+  );
+
+  trackById(index: number, state: State) {
+    return state.code + index;
+  }
 
   constructor(private service: CountryService) {}
+
 }
