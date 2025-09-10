6a790c8dd7529eb90693345ba590ad852452db88
igor2000xp <igorring@google.com>
2025-09-10 06:28:28 +0200

    chore: Update Angular cache files and enhance exercise4 component with reactive form controls for state selection and dynamic filtering

--- src/app/exercise4/exercise4.component.ts
+++ src/app/exercise4/exercise4.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, withLatestFrom } from 'rxjs/operators';
+import { combineLatest, Observable, Subject } from 'rxjs';
+import { map, startWith, switchMap, withLatestFrom } from 'rxjs/operators';
 import { CountryService } from './country.service';
 import { Country, State } from './types';
 
@@ -13,8 +13,12 @@ import { Country, State } from './types';
 export class Exercise4Component {
   countries$: Observable<Country[]>;
   states$!: Observable<State[]>;
+  currentCountry$ = new Subject<Country>();
+  statesForCountrie$: Observable<State[]>;
+
   state!: State;
-  countryControl = new FormControl<string>('');
+  countryControl = new FormControl<string>('', {nonNullable: true});
+  stateControl = new FormControl<string>('', {nonNullable: true});
 
   constructor(private service: CountryService) {
     this.countries$ = this.countryControl.valueChanges.pipe(
@@ -23,10 +27,27 @@ export class Exercise4Component {
         countries.filter((c) => c.description.toLowerCase().indexOf((userInput ?? '').toLowerCase()) !== -1),
       ),
     );
+
+    this.statesForCountrie$ = this.currentCountry$.pipe(
+      switchMap((country) => this.service.getStatesFor(country.id))
+    );
+
+    this.states$ = combineLatest([
+      this.stateControl.valueChanges.pipe(startWith(this.stateControl.value)),
+      this.statesForCountrie$
+    ]).pipe(
+      map(([userInput, states]) => {
+        return states.filter((s) => {
+          return s.description.toLowerCase().includes(userInput);
+        })
+      })
+    );
   }
 
   updateStates(country: Country) {
     this.countryControl.setValue(country.description);
-    this.states$ = this.service.getStatesFor(country.id);
+    // this.states$ = this.service.getStatesFor(country.id);
+    this.stateControl.setValue('');
+    this.currentCountry$.next(country);
   }
 }
